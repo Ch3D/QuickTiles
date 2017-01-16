db649f517cc1f164774bbf432c044cc6e7c81238
Dmitry Chertenko <ch3dee@gmail.com>
2017-01-16 21:56:00 +0100

    Go Kotlin

---
+++ app/src/main/java/com/ch3d/android/quicktiles/TileState.kt
@@ -0,0 +1,9 @@
+package com.ch3d.android.quicktiles
+
+/**
+ * Created by Dmitry on 6/20/2016.
+ */
+data class TileState(val drawableId: Int,
+                     val primaryValue: Int,
+                     val secondaryValue: Int,
+                     val titleResId: Int)
