fb672f6ab8e0e71b99ffd781beb5eb64ad0eaaf9
Dmitry Chertenko <ch3dee@gmail.com>
2017-01-16 22:24:19 +0100

    Kotlin-style builder pattern implementation

--- app/src/main/java/com/ch3d/android/quicktiles/TileState.kt
+++ app/src/main/java/com/ch3d/android/quicktiles/TileState.kt
@@ -1,5 +1,7 @@
 package com.ch3d.android.quicktiles
 
+import android.service.quicksettings.Tile
+
 /**
  * Created by Dmitry on 6/20/2016.
  */
@@ -7,4 +9,26 @@ data class TileState(val state: Int,
                      val drawableId: Int,
                      val primaryValue: Int,
                      val secondaryValue: Int,
-                     val titleResId: Int)
+                     val titleResId: Int) {
+
+    private constructor(builder: Builder) :
+            this(builder.state,
+                    builder.drawableId,
+                    builder.primaryValue,
+                    builder.secondaryValue,
+                    builder.titleResId)
+
+    companion object {
+        inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
+    }
+
+    class Builder {
+        var state: Int = Tile.STATE_INACTIVE
+        var drawableId: Int = 0
+        var primaryValue: Int = 0
+        var secondaryValue: Int = 0
+        var titleResId: Int = 0
+
+        fun build() = TileState(this)
+    }
+}
